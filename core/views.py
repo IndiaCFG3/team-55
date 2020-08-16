41cb0e6ad35232bb430583fdb90bb1b3568db21a
Khush Jain <khush4080@gmail.com>
2020-08-16 09:34:33 +0530

    Initialised Project

---
+++ core/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+
+@login_required(login_url="account/login")	
+def home(request):
+	return render(request, 'core/home.html')
+
+
+def error_404_view(request, exception):
+	return render(request, '404.html')
